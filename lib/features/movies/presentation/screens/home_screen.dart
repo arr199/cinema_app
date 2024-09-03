170581ddd2d5337016d631c8544743a8fa1aaf0d
arr199 <abieledelso@gmail.com>
2024-09-04 00:50:31 +0100

    display movies

--- lib/features/movies/presentation/screens/home_screen.dart
+++ lib/features/movies/presentation/screens/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:cinema_app/features/movies/presentation/providers/movie_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class HomeScreen extends StatelessWidget {
   static const name = 'home_screen';
@@ -8,9 +10,40 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(
-        child: Text("HELLO"),
-      ),
+      body: _HomeView(),
     );
+  }
+}
+
+class _HomeView extends ConsumerStatefulWidget {
+  const _HomeView({
+    super.key,
+  });
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends ConsumerState<_HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    final movies = ref.watch(nowPlayingMoviesProvider);
+
+    return ListView.builder(
+      itemCount: movies.length,
+      itemBuilder: (context, index) {
+        final movie = movies[index];
+        return ListTile(
+          title: Text(movie.title),
+          subtitle: Text(movie.overview),
+        );
+      },
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
   }
 }
